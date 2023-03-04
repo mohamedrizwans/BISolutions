ff3bd9d488a0e33c6a9c01a31adf499aa623fac6
alyasamailboxes <alyasamailboxes@gmail.com>
2023-03-04 22:12:54 +0300

    .

---
+++ Google Sheet and MySQL/main.py
@@ -0,0 +1,6 @@
+import gsmysql as gsm
+
+gsm.Runner
+# Runner(SheetKey, SheetName, mysqlTable, SheetColumnCount)
+gsm.Runner('1zDKhh4YJOCxfgudwXcd7KTrmu_9tkRMwZtGSV_5XV34', "Purchase", 'tInvPurchase', 22)
+gsm.Runner('1zDKhh4YJOCxfgudwXcd7KTrmu_9tkRMwZtGSV_5XV34', "Outward", 'tInvOutward', 15)
