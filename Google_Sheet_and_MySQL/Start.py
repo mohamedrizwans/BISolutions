8fb69d30f093cb805259d5fb2fce51b39fe0b131
rafimailboxes <rafimailboxes@gmail.com>
2023-03-11 22:30:22 +0300

    .

--- Google_Sheet_and_MySQL/Start.py
+++ Google_Sheet_and_MySQL/Start.py
@@ -4,40 +4,44 @@
 import gsmysql as gsm
 import mysqlgs as mgs
 import json
-  
+
 # Opening JSON file
 f = open('Config.json')
 
-# returns JSON object as 
+# returns JSON object as
 # a dictionary
 data = json.load(f)
-  
+
 # Load Data from Google Sheet to Database
-for i in data:        
-        # gsm.connectdb(i["dbcred"]["host"],i["dbcred"]["database"],i["dbcred"]["user"],i["dbcred"]["password"])
-        for j in i["gstodb"]:
-                sheetid = j["sheetid"]
-                for k in j["gstabs"]:
-                        gstab = k["gstab"]
-                        dbtab = k["dbtab"]
-                        columncount = k["columncount"]
-                        gsm.Runner(i["dbcred"]["host"],i["dbcred"]["database"],i["dbcred"]["user"],i["dbcred"]["password"],
-                                   sheetid, gstab, dbtab, columncount)
+for i in data:
+    # gsm.connectdb(i["dbcred"]["host"],i["dbcred"]["database"],i["dbcred"]["user"],i["dbcred"]["password"])
+    print(i["client"])
+    print("------------------")
+    if i["isactive"] == False:
+        print("Client is Disabled. So, Moving to Next client")
+        continue
+    for j in i["gstodb"]:
+        sheetid = j["sheetid"]
+        for k in j["gstabs"]:
+            gstab = k["gstab"]
+            dbtab = k["dbtab"]
+            columncount = k["columncount"]
+            gsm.Runner(i["dbcred"]["host"], i["dbcred"]["database"], i["dbcred"]["user"], i["dbcred"]["password"],
+                       sheetid, gstab, dbtab, columncount)
 
 
 # Load Data from Database to Google Sheet
-for i2 in data:        
-        # gsm.connectdb(i["dbcred"]["host"],i["dbcred"]["database"],i["dbcred"]["user"],i["dbcred"]["password"])
-        for j2 in i2["dbtogs"]:
-                sheetid = j2["sheetid"]
-                for k2 in j2["gstabs"]:
-                        gstab = k2["gstab"]
-                        dbtab = k2["dbtab"]                        
-                        mgs.Runner(i2["dbcred"]["host"],i2["dbcred"]["database"],i2["dbcred"]["user"],i2["dbcred"]["password"],
-                                   sheetid, gstab, dbtab)
-                        
-
-
+for i2 in data:
+    print(i2["client"])
+    print("------------------")
+    # gsm.connectdb(i["dbcred"]["host"],i["dbcred"]["database"],i["dbcred"]["user"],i["dbcred"]["password"])
+    for j2 in i2["dbtogs"]:
+        sheetid = j2["sheetid"]
+        for k2 in j2["gstabs"]:
+            gstab = k2["gstab"]
+            dbtab = k2["dbtab"]
+            mgs.Runner(i2["dbcred"]["host"], i2["dbcred"]["database"], i2["dbcred"]["user"], i2["dbcred"]["password"],
+                       sheetid, gstab, dbtab)
 
 
 # Closing file
